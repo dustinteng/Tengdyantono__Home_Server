cc51d7960c1cf08962e7627a0e4de12018d5d242
dustinteng <jtengdya@ucsd.edu>
2021-02-11 14:45:24 +0700

    adding webpage reset timer

---
+++ raspberry pi code/webpage_reset_timer.py
@@ -0,0 +1,19 @@
+#this code is meant to reset the server every 30 minutes, to prevent 
+#the webpage to be 'freezed' this code let users to not have to deal with 
+#coding / even resetting the server. 
+
+#imports
+import webpage 
+import time
+import os
+
+#variable initalization
+minutes = 30
+seconds = minutes * 60
+while True:
+    os.system("clear")
+    os.system("pm2 restart 1") #smart / lazy :)
+    print("website is running")
+    print("reset every " + str(minutes) + " minutes")
+    time.sleep(seconds) #change the front to set minutes
+    
